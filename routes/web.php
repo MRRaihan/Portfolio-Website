37c7a229e21c36c87ff3d0dc4e023405a076ff9f
mizanur <mizanurahmed1452@gmail.com>
2020-12-13 01:49:39 +0600

    Frontend add

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes([
     'register' => false, // Registration Routes...
@@ -24,6 +21,9 @@ Auth::routes([
 ]);
 
 /*Route::get('/home', 'HomeController@index')->name('home');*/
+
+
+Route::get('/', 'HomeController@index')->name('home');
 
 
 Route::group(['prefix'=>'admin', 'middleware'=>'auth'],function (){
